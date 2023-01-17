d7cacf01f5d41d6de6ff8d60feeed3a12f758b35
Santiago Orozco <devsos117@gmail.com>
2023-01-16 22:00:07 -0600

    Clase 1

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,38 @@
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+<template>
+    <div>
+        <h1>{{ producto }}</h1>
+        <p>Precio: {{ precio }}(iva16%:{{ precioCalculado }})</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            producto: "Hello world",
+            precio: 100,
+            iva: 1.16,
+        }
+    },
+    methods: {
+        diHola() {
+            console.log("hola");
+        }
+    },
+    computed: {
+        precioCalculado() {
+            console.log("se recalculo el precio")
+            return this.precio * this.iva;
+        }
+    },
+    watch: {
+        precio(newVal, oldVal) {
+            console.log(`se actualizo el precio de ${oldVal} a ${newVal}`);
+        }
+    },
+    
+};
 </script>
 
-<template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
-            </div>
-        </div>
-    </AppLayout>
-</template>
+<style>
+</style>
